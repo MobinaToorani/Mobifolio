5caa929ff6cec13bfeaaaf5f2ab7274cb08c0592
Mobina Tooranisama <mobinatoorani@gmail.com>
2022-06-07 14:46:16 -0400

    Add skeleton files and basic css

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	
+</script>
+
+<main>
+	<!-- Gotta fix the icon before placing-->
+	<!-- <img src="/favicon/apple-touch-icon.png" alt="Mobifolio Logo" width="315" height="315">  -->
+
+	<div id="container">
+
+	</div>
+	
+</main>
+
+<style>
+	/* Change background color (#191414) */
+	:global(body) {
+		background-color: #191414;
+	}
+
+	/* Mobile */
+	#container {
+		/*Create border for container*/
+		border: 1px solid #1DB954;
+
+		/*Change border radius*/
+		border-radius: 5px;
+
+		/*Change padding*/
+		padding: 4%;
+
+		/*Change margin*/
+		margin: 10px;
+	}
+
+	/* Tablet */
+	@media only screen and (max-width: 600px) {
+		#container {
+			border: 1px solid #1DB954;
+			border-radius: 5px;
+			padding: 3%;
+			margin: 10px;
+		}
+	}
+
+	/* Desktop */
+	@media only screen and (max-width: 900px) {
+		#container {
+			border: 1px solid #1DB954;
+			border-radius: 5px;
+			padding: 2%;
+			margin: 10px;
+		}
+	}
+</style>
